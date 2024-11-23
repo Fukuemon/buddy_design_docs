350f20108ce1aae7fc0bc3eb2dda9d8459a0aa20
Fukuemon <Fukuemony029@gmail.com>
2024-11-23 11:09:04 +0900

    ER図の更新

--- docker/convert_puml.sh
+++ docker/convert_puml.sh
@@ -2,16 +2,18 @@
 
 # diagramsディレクトリ内のすべてのpumlファイルを処理
 find /app/src/diagrams -name "*.puml" | while read puml_file; do
-    # src/diagrams内の相対パスを取得
+    # 相対パスを取得
     relative_path="${puml_file#/app/src/diagrams/}"
-    # svgファイルの保存先と名前を設定
+    echo relative_path: $relative_path
+    # svgファイルの保存先を決定
     svg_file="/app/src/diagrams-svg/${relative_path%.puml}.svg"
-    # 保存先ディレクトリを作成（存在しない場合のみ）
+    # 保存先ディレクトリを作成
     mkdir -p "$(dirname "$svg_file")"
-    # pumlファイルをsvgに変換（デフォルト出力をリネーム）
-    echo "Processing $puml_file"
-    java -DPLANTUML_LIMIT_SIZE=32768 -Dplantuml.font.notosans="Noto Sans" \
-        -jar /usr/local/bin/plantuml.jar -tsvg "$puml_file"
-    mv "$(dirname "$puml_file")/entity-relationship-diagram.svg" "$svg_file"
+    # pumlファイルをsvgに変換して一時的に元の場所に保存
+    plantuml -tsvg "$puml_file"
+    # 元のディレクトリで作成されたSVGファイルを取得
+    generated_svg=$(find "$(dirname "$puml_file")" -name "*.svg" -print -quit)
+    # SVGファイルを目的のディレクトリに移動
+    mv "$generated_svg" "$svg_file"
     echo "Converted $puml_file to $svg_file"
 done
